5e771a76653f3380bed627012913e4d38bf63c14
Alex H <alexander.hawley@betterment.com>
2024-10-04 12:01:52 -0400

    add todos

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,18 @@ import { BoardComponent } from './components/BoardComponent';
 
 const App = () => {
   console.log('app render');
+  // if (currentPlayer.role == spymaster) {
+  //   getGameState(currentPlayer)
+  //   {cards: []}
+  //   cards include color/side
+  // }
+  // if (currentPlayer.role == operative) {
+  //   getGameState(currentPlayer)
+  //   {cards: []}
+  //   cards only include color/side if already flipped
+  // }
+  // flipCard(index, currentPlayer)
+  // => all players notified & get new game state
   return (
     <>
       <BoardComponent />
