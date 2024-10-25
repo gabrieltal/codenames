73b3a487197bc1b2bd08c2005898450c31f6dc57
Alex H <alexander.hawley@betterment.com>
2024-10-25 14:39:27 -0400

    make client use server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,16 @@
+import { ApolloClient, InMemoryCache, OperationVariables, gql } from "@apollo/client/core/index.js";
 import './App.css'
 import { BoardComponent } from './components/BoardComponent';
+import { useEffect } from "react";
+import { Card } from "./models/Card";
+
+interface Data {
+  gameState: GameState;
+};
+
+interface GameState {
+  board: Array<Card>;
+};
 
 const App = () => {
   console.log('app render');
@@ -15,6 +26,34 @@ const App = () => {
   // }
   // flipCard(index, currentPlayer)
   // => all players notified & get new game state
+
+  useEffect(() => {
+    console.log("App.load");
+    (async () => {
+      console.log("App.load.query start");
+      const client = new ApolloClient({
+        uri: "/api/graphql",
+        cache: new InMemoryCache(),
+      });
+      const {data} = await client.query<Data, OperationVariables>({query: gql`
+        query GameState {
+          gameState {
+            board {
+              color
+              flipped
+              word
+            }
+          }
+        }
+      `});
+      console.log(`data: ${JSON.stringify(data, null, 2)}`);
+      console.log(`data.gameState.board: ${data.gameState.board.length}`);
+      console.log(`data.gameState.board[0].word: ${data.gameState.board[0].word}`);
+      console.log("App.load.query done");
+    })();
+    console.log("App.load done");
+  }, []);
+  
   return (
     <>
       <BoardComponent />
